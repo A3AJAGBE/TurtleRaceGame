dd84a531b7ead67312e119aca9db144c0ad3a865
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-04 05:41:54 +0000

    Add the instances turtle

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import turtle
 
-sample = turtle.Turtle()
 screen = turtle.Screen()
-
 screen.title('Turtle Race Game')
 screen.setup(width=500, height=400)
 
-sample.shape('turtle')
+
+colors = ['pink', 'blue', 'green', 'brown', 'red']
+posX = -230
+posY = -150
+for color in colors:
+    sample = turtle.Turtle(shape='turtle')
+    sample.color(color)
+    sample.penup()
+    posY += 50
+    sample.goto(x=posX, y=posY)
+
+bet = screen.textinput(title='Make your Bet', prompt='Enter the color of the turtle you support')
+print(bet)
 
 
 screen.exitonclick()
