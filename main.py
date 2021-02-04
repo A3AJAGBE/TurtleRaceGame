159a052b976d7fc56592216dd686c3553c83454f
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-04 05:22:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import turtle
+
+sample = turtle.Turtle()
+screen = turtle.Screen()
+
+screen.title('Turtle Race Game')
+screen.setup(width=500, height=400)
+
+sample.shape('turtle')
+
+
+screen.exitonclick()
