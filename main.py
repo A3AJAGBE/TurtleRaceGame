63a69dca06bf3bbe857e2c6de9009c820442a59d
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-04 06:05:30 +0000

    Add race function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import turtle
 
 screen = turtle.Screen()
@@ -8,15 +9,33 @@ screen.setup(width=500, height=400)
 colors = ['pink', 'blue', 'green', 'brown', 'red']
 posX = -230
 posY = -150
+race_turtles = []
 for color in colors:
     sample = turtle.Turtle(shape='turtle')
     sample.color(color)
     sample.penup()
     posY += 50
     sample.goto(x=posX, y=posY)
+    race_turtles.append(sample)
+
+race_start = False
 
 bet = screen.textinput(title='Make your Bet', prompt='Enter the color of the turtle you support')
 print(bet)
 
+if bet:
+    race_start = True
+
+while race_start:
+    for race_turtle in race_turtles:
+        if race_turtle.xcor() > 230:
+            race_start = False
+            winner = race_turtle.pencolor()
+            if winner == bet:
+                print(f'You won, {winner} turtle won the race.')
+            else:
+                print(f'You lost, {winner} turtle won the race.')
+        distance = random.randint(0, 10)
+        race_turtle.forward(distance)
 
 screen.exitonclick()
